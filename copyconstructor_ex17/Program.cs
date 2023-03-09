5d24d084ab403b7eb4bb9ebe51d2d5b2ed9320dd
wasimakram-fly <wasimakram8099@gmail.com>
2023-03-09 18:41:40 +0530

    class created

---
+++ copyconstructor_ex17/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace copyconstructor_ex17
+{
+    internal class Program
+    {
+        public class student
+        {
+            int sno;
+            string sname;
+
+            public student()    // user defined default constructor.
+            {
+                sno = 101;
+                sname = "smith";
+            }
+
+            public student(student obj)     // copy constructor.
+            {
+                sno = obj.sno;
+                sname = obj.sname;
+            }
+
+            public void display()
+            {
+                Console.WriteLine("Student number: " + sno);
+                Console.WriteLine("Student Name: " + sname);
+            }
+        }
+        static void Main(string[] args)
+        {
+            student std = new student();
+            std.display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ staticconstructor_ex18/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticconstructor_ex18
+{
+    public class a
+        
+    {
+        static a()            
+        {
+           Console.WriteLine("I am a static constructor.");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am a static Main() method constructor.");
+            Console.ReadLine();
+        }
+    }
+}
